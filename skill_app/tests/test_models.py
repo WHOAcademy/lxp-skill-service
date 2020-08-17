2576997f04cff29799543ad5a6b116437d7dcc42
Aykut Bulgu <mabulgu@gmail.com>
2020-08-17 23:27:46 +0300

    add get all test

--- skill_app/tests/test_models.py
+++ skill_app/tests/test_models.py
@@ -5,3 +5,11 @@ from skill_app.models import SkillModel, RoleModel
 class TestRoleModel(TestCase):
     def test_create_role(self):
         RoleModel.objects.create(name="Title", slug="title")
+
+    def test_get_all(self):
+        RoleModel.objects.create(title="Title1", slug="title-1")
+        RoleModel.objects.create(title="Title2", slug="title-2")
+        RoleModel.objects.create(title="Title3", slug="title-3")
+
+        courses = RoleModel.objects.all()
+        self.assertEqual(len(courses), 3)
