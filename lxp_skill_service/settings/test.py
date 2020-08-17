785fd1f410723666fd35aa2a4a6a34c861523f57
Aykut Bulgu <mabulgu@gmail.com>
2020-08-18 00:18:11 +0300

    fix typo

--- lxp_skill_service/settings/test.py
+++ lxp_skill_service/settings/test.py
@@ -1,7 +1,7 @@
 from .base import *
 import importlib
 
-settings = importlib.import_module('lxp_skills_service.settings.' + os.getenv('APP_ENV'))
+settings = importlib.import_module('lxp_skill_service.settings.' + os.getenv('APP_ENV'))
 
 settings.DATABASES['default']['HOST'] = os.getenv('TEST_DATABASE_SERVICE_HOST')
 settings.DATABASES['default']['PORT'] = os.getenv('TEST_DATABASE_SERVICE_PORT')
