2e4a31d1b1d6f89c992a18ba1df48f6ff7ed0f2e
Aykut Bulgu <mabulgu@gmail.com>
2020-08-18 00:16:54 +0300

    fix db choice

--- lxp_skill_service/settings/test.py
+++ lxp_skill_service/settings/test.py
@@ -1,4 +1,9 @@
 from .base import *
+import importlib
 
-DATABASES['default']['HOST'] = os.getenv('TEST_DATABASE_SERVICE_HOST')
-DATABASES['default']['PORT'] = os.getenv('TEST_DATABASE_SERVICE_PORT')
+settings = importlib.import_module('lxp_skills_service.settings.' + os.getenv('APP_ENV'))
+
+settings.DATABASES['default']['HOST'] = os.getenv('TEST_DATABASE_SERVICE_HOST')
+settings.DATABASES['default']['PORT'] = os.getenv('TEST_DATABASE_SERVICE_PORT')
+
+DATABASES = settings.DATABASES
