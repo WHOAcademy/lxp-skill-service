ee572f44e1b6bb65ac08da480a0704d0c17c83dd
Aykut Bulgu <mabulgu@gmail.com>
2020-08-17 17:49:35 +0300

    change host to ip - temp

--- lxp_skill_service/settings/test.py
+++ lxp_skill_service/settings/test.py
@@ -7,7 +7,7 @@ DATABASES = {
         'NAME': os.getenv('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': 'test-postgresql',
+        'HOST': '172.30.105.228',
         'PORT': os.getenv('DATABASE_SERVICE_PORT'),
     }
 }
