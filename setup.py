57d55e192bd36830bae0a91a26b82619fa3e507a
Aykut Bulgu <mabulgu@gmail.com>
2020-08-18 16:04:17 +0300

    bump app version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='lxp-skill-service',
-      version='1.1.1',
+      version='1.2.0',
       packages=find_packages(),
       install_requires=[
           'Django~=3.0.8',
