9f1e3f991d444d719fe4f41d02e5c66f91febd4a
Krishna-Madhavan-07 <krishnamadhavan07@gmail.com>
2020-08-24 19:31:09 +0530

    🤪 Missed = on setup.py 🤯

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='lxp-skill-service',
           'django-nose==1.4.6',
           'drf-yasg==1.17.1',
           'django-cors-headers==3.4.0',
-          'django-opentracing=1.1.0',
+          'django-opentracing==1.1.0',
           'jaeger-client==4.3.0',
           'opentracing==2.3.0'
       ],
