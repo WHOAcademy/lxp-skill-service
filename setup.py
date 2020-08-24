c0ba749a98219758528be7f95d9ed4ca9bc48385
Mahendran Kannan <gkm09@hotmail.com>
2020-08-24 19:33:57 +0530

    💻 ADD - health check routes 💻

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='lxp-skill-service',
-      version='1.2.2',
+      version='1.3.0',
       packages=find_packages(),
       install_requires=[
           'Django~=3.0.8',
